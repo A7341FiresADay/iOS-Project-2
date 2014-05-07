46f20f4081b1b76b6c395544a90141f153f3ad0a
Andrew <amh2644@g.rit.edu>
2014-05-07 17:03:15 -0400

    Corey added moving background and image

--- Project2/Project2/MyScene.m
+++ Project2/Project2/MyScene.m
@@ -9,12 +9,32 @@
 #import "MyScene.h"
 #import "GameOverScene.h"
 
+static inline CGPoint CGPointAdd(const CGPoint a,
+                                 const CGPoint b)
+{
+    return CGPointMake(a.x + b.x, a.y + b.y);
+}
+
+static inline CGPoint CGPointMultiplyScalar(const CGPoint a,
+                                            const CGFloat b)
+{
+    return CGPointMake(a.x * b, a.y * b);
+}
+
+static const float BG_POINTS=50;
 @implementation MyScene
+{
+    NSTimeInterval _lastUpdateTime;
+    NSTimeInterval _dt;
+    SKNode *_bglayer;
+ 
+}
 
 -(id)initWithSize:(CGSize)size {    
     if (self = [super initWithSize:size]) {
         /* Setup your scene here */
-        
+        _bglayer=[SKNode node];
+        [self addChild:_bglayer];
         self.backgroundColor = [SKColor colorWithRed:0.15 green:0.15 blue:0.3 alpha:1.0];
         
         SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Menlo-Regular"];
@@ -25,6 +45,16 @@
                                        CGRectGetMidY(self.frame));
         
         [self addChild:myLabel];
+        for(int i =0; i<2;i++)
+        {
+            SKSpriteNode *bg= [SKSpriteNode spriteNodeWithImageNamed:@"background"];
+            bg.anchorPoint = CGPointZero;
+            bg.position = CGPointMake(i *bg.size.width, 0);
+            bg.name =@"bg";
+            [self addChild:bg];
+            
+            //look at chapter 5 page 136-141 for help about layers and correct positions of stuff
+        }
     }
     return self;
 }
@@ -37,6 +67,28 @@
 
 -(void)update:(CFTimeInterval)currentTime {
     /* Called before each frame is rendered */
+    if (_lastUpdateTime) {
+        _dt = currentTime - _lastUpdateTime;
+    } else {
+        _dt = 0;
+    }
+     _lastUpdateTime = currentTime;
+    [self moveBG];
+}
+//moves the background and recycles it so it loops forever
+-(void)moveBG
+{
+    [self enumerateChildNodesWithName:@"bg" usingBlock:^(SKNode *node, BOOL *stop)
+     {
+         SKSpriteNode *bg = (SKSpriteNode *)node;
+         CGPoint bgVelocity = CGPointMake(-BG_POINTS,0);
+         CGPoint amtToMove= CGPointMultiplyScalar(bgVelocity, _dt);
+         bg.position = CGPointAdd(bg.position,amtToMove);
+         if(bg.position.x<= -bg.size.width)
+         {
+             bg.position = CGPointMake(bg.position.x +bg.size.width*2, bg.position.y);
+         }
+     }];
 }
 
 @end
